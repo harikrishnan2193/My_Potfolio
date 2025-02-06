103eb50474435b00c024a31c3c87d24ff73c4c0a
harikrishnan2193 <harikrishnank2193@gmail.com>
2025-02-06 15:12:32 +0530

    update resume file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Experience from '../components/Experience'
 import Contact from '../components/Contact'
-import resume from '../assets/Harikrishnan-Resume.pdf'
+import resume from '../assets/Harikrishnan_Resume.pdf'
 import image from '../images/image blur.jpg'
 
 function Home() {
